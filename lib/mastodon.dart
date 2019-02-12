8ae83c60cf82d27e9f3779001c11a14e933073ab
Luke Pighetti <lukepighetti@gmail.com>
2019-02-12 15:03:02 -0500

    export data classes

--- lib/mastodon.dart
+++ lib/mastodon.dart
@@ -1,2 +1,21 @@
 /// Mastodon library documentation goes here.
 library mastodon;
+
+/// Data classes
+export 'data/account.dart';
+export 'data/application.dart';
+export 'data/attachment.dart';
+export 'data/card.dart';
+export 'data/context.dart';
+export 'data/emoji.dart';
+export 'data/filter.dart';
+export 'data/history.dart';
+export 'data/instance.dart';
+export 'data/mention.dart';
+export 'data/notification.dart';
+export 'data/push_subscription.dart';
+export 'data/relationship.dart';
+export 'data/results.dart';
+export 'data/scheduled_status.dart';
+export 'data/status.dart';
+export 'data/tag.dart';

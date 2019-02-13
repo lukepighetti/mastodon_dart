6562f29619bcedb9cc82444381338d6259d59336
Luke Pighetti <lukepighetti@gmail.com>
2019-02-12 19:58:25 -0500

    Domain Blocks endpoint

---
+++ lib/mock/mixins/domain_blocks.dart
@@ -0,0 +1,16 @@
+import '../properties.dart';
+
+class MockDomainBlocksMixin {
+  /// GET /api/v1/domain_blocks
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#get-api-v1-domain-blocks
+  Future<List<Uri>> domainBlocks() =>
+      Future.value(List.generate(5, (_) => MockProperties.uri));
+
+  /// POST /api/v1/domain_blocks
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#post-api-v1-domain-blocks
+  Future<dynamic> domainBlock(Uri domain) => Future.value(null);
+
+  /// DELETE /api/v1/domain_blocks
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#delete-api-v1-domain-blocks
+  Future<dynamic> domainUnblock(Uri domain) => Future.value(null);
+}

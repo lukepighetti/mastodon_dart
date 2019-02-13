cbda50410eee95aa3c1976d91bddab5d1f0a4460
Luke Pighetti <lukepighetti@gmail.com>
2019-02-12 20:02:09 -0500

    Endorsements endpoint

---
+++ lib/mock/mixins/endorsements.dart
@@ -0,0 +1,18 @@
+import 'package:mastodon/mastodon.dart';
+
+class MockEndorsementsMixin {
+  /// GET /api/v1/endorsements
+  /// https://docs.joinmastodon.org/api/rest/endorsements/#get-api-v1-endorsements
+  ///
+  /// TODO: implement link headers for pagination
+  Future<List<Account>> endorsements() =>
+      Future.value(List.generate(20, (_) => Account.mock()));
+
+  /// POST /api/v1/accounts/:id/pin
+  /// https://docs.joinmastodon.org/api/rest/endorsements/#post-api-v1-accounts-id-pin
+  Future<Relationship> pin(String id) => Future.value(Relationship.mock());
+
+  /// POST /api/v1/accounts/:id/unpin
+  /// https://docs.joinmastodon.org/api/rest/endorsements/#post-api-v1-accounts-id-unpin
+  Future<Relationship> unpin(String id) => Future.value(Relationship.mock());
+}

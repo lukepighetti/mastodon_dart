85baf33970330ec55fb21840133c33275a4ce671
Luke Pighetti <lukepighetti@gmail.com>
2019-02-12 19:52:24 -0500

    Custom Emojis endpoints

---
+++ lib/mock/mixins/custom_emojis.dart
@@ -0,0 +1,8 @@
+import 'package:mastodon/mastodon.dart';
+
+class MockCustomEmojisMixin {
+  /// GET /api/v1/custom_emojis
+  /// https://docs.joinmastodon.org/api/rest/custom-emojis/#get-api-v1-custom-emojis
+  Future<List<Emoji>> emojis() =>
+      Future.value(List.generate(5, (_) => Emoji.mock()));
+}

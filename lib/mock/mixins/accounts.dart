a072964895f1034d25720611b0ed124eb47befdb
Luke Pighetti <lukepighetti@gmail.com>
2019-02-13 19:49:04 -0500

    Add endpoint urls to MockAccountsMixin

--- lib/mock/mixins/accounts.dart
+++ lib/mock/mixins/accounts.dart
@@ -3,9 +3,11 @@ import 'package:meta/meta.dart';
 
 class MockAccountsMixin {
   /// GET /api/v1/accounts/:id
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id
   Future<Account> account(String id) => Future.value(Account.mock());
 
   /// POST /api/v1/accounts
+  /// https://docs.joinmastodon.org/api/rest/accounts/#post-api-v1-accounts
   Future<Token> token({
     @required String username,
     @required String email,
@@ -16,9 +18,11 @@ class MockAccountsMixin {
       Future.value(Token.mock());
 
   /// GET /api/v1/accounts/verify_credentials
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-verify-credentials
   Future<Account> verifyCredentials() => Future.value(Account.mock());
 
   /// PATCH /api/v1/accounts/update_credentials
+  /// https://docs.joinmastodon.org/api/rest/accounts/#patch-api-v1-accounts-update-credentials
   Future<Account> updateCredentials({
     String displayName,
     String note,
@@ -33,14 +37,17 @@ class MockAccountsMixin {
       Future.value(Account.mock());
 
   /// GET /api/v1/accounts/:id/followers
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id-followers
   Future<List<Account>> followers(String id, {int limit = 40}) =>
       Future.value(List.generate((limit), (_) => Account.mock()));
 
   /// GET /api/v1/accounts/:id/following
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id-following
   Future<List<Account>> following(String id, {int limit = 40}) =>
       Future.value(List.generate((limit), (_) => Account.mock()));
 
   /// GET /api/v1/accounts/:id/statuses
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id-statuses
   Future<List<Status>> statuses(
     String id, {
     bool onlyMedia = false,
@@ -55,17 +62,21 @@ class MockAccountsMixin {
       Future.value(List.generate((limit), (_) => Status.mock()));
 
   /// POST /api/v1/accounts/:id/follow
+  /// https://docs.joinmastodon.org/api/rest/accounts/#post-api-v1-accounts-id-follow
   Future<Relationship> follow(String id, {bool reblogs = true}) =>
       Future.value(Relationship.mock());
 
   /// POST /api/v1/accounts/:id/unfollow
+  /// https://docs.joinmastodon.org/api/rest/accounts/#post-api-v1-accounts-id-unfollow
   Future<Relationship> unfollow(String id) => Future.value(Relationship.mock());
 
   /// GET /api/v1/accounts/relationships
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-relationships
   Future<List<Relationship>> relationships(List<String> ids) =>
       Future.value(List.generate((ids.length), (_) => Relationship.mock()));
 
   /// GET /api/v1/accounts/search
+  /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-search
   Future<List<Account>> searchAccounts(
     String q, {
     int limit = 40,

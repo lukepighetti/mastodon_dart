a11e51b999003929bcfe0677b66766398c9de12b
Luke Pighetti <lukepighetti@gmail.com>
2019-02-18 10:13:31 -0500

    library

--- lib/src/mixins/apps.dart
+++ lib/src/mixins/apps.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:meta/meta.dart';
+import '../library.dart';
 
-import 'package:mastodon/mastodon.dart';
 import 'package:mastodon/mock/mixins/apps.dart';
 
-mixin AppsMixin on Authentication implements MockAppsMixin {
+mixin Apps on Authentication implements MockAppsMixin {
   /// POST /api/v1/apps
   /// https://docs.joinmastodon.org/api/rest/apps/#post-api-v1-apps
   Future<AuthenticatedApplication> appCredentials({
@@ -14,7 +11,7 @@ mixin AppsMixin on Authentication implements MockAppsMixin {
     List<String> scopes = const ["write", "read", "follow", "push"],
     @required Uri website,
   }) async {
-    final response = await http.post(
+    final response = await post(
       "$baseUrl/api/v1/apps",
       body: {
         "client_name": clientName,
@@ -32,7 +29,7 @@ mixin AppsMixin on Authentication implements MockAppsMixin {
   Future<Application> verifyAppCredentials() async {
     assert(key != null);
 
-    final response = await http.get(
+    final response = await get(
       "$baseUrl/api/v1/apps/verify_credentials",
       headers: {"Authorization": "Bearer $key"},
     );
@@ -40,14 +37,3 @@ mixin AppsMixin on Authentication implements MockAppsMixin {
     return Application.fromJson(json.decode(response.body));
   }
 }
-
-// import 'package:http/http.dart' as http;
-
-// var url = "http://example.com/whatsit/create";
-// http.post(url, body: {"name": "doodle", "color": "blue"})
-//     .then((response) {
-//   print("Response status: ${response.statusCode}");
-//   print("Response body: ${response.body}");
-// });
-
-// http.read("http://example.com/foobar.txt").then(print);

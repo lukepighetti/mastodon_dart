6c7a6a3d90d4055970019f7f82fa7eca462cf443
Luke Pighetti <lukepighetti@gmail.com>
2019-02-25 13:21:29 -0500

    Accounts.account, .token, .verifyCredentials, .updateCredentials

--- lib/src/mixins/accounts.dart
+++ lib/src/mixins/accounts.dart
@@ -4,10 +4,30 @@ import 'package:mastodon/mock/mixins/accounts.dart';
 
 mixin Accounts on Authentication implements MockAccountsMixin {
   /// GET /api/v1/accounts/:id
+  ///
+  /// - public
+  /// - read read:accounts
+  ///
   /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id
-  Future<Account> account(String id) => Future.value(Account.mock());
+  Future<Account> account(String id) async {
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/accounts/$id",
+    );
+
+    final response = await get(
+      uri,
+    );
+
+    return Account.fromJson(json.decode(response.body));
+  }
 
   /// POST /api/v1/accounts
+  ///
+  /// - authenticated (no user required)
+  /// - write write:accounts
+  ///
   /// https://docs.joinmastodon.org/api/rest/accounts/#post-api-v1-accounts
   Future<Token> token({
     @required String username,
@@ -15,14 +35,58 @@ mixin Accounts on Authentication implements MockAccountsMixin {
     @required String password,
     @required bool agreement,
     @required String locale,
-  }) =>
-      throw UnimplementedError();
+  }) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/accounts",
+      queryParameters: {
+        "username": username,
+        "email": email,
+        "password": password,
+        "agreement": agreement.toString(),
+        "locale": locale,
+      },
+    );
+
+    final response = await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Token.fromJson(json.decode(response.body));
+  }
 
   /// GET /api/v1/accounts/verify_credentials
+  ///
+  /// - authenticated (requires user)
+  /// - read read:accounts
+  ///
   /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-verify-credentials
-  Future<Account> verifyCredentials() => throw UnimplementedError();
+  Future<Account> verifyCredentials() async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/accounts/verify_credentials",
+    );
+
+    final response = await get(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Account.fromJson(json.decode(response.body));
+  }
 
   /// PATCH /api/v1/accounts/update_credentials
+  ///
+  /// - authenticated (requires user)
+  /// - write write:accounts
+  ///
   /// https://docs.joinmastodon.org/api/rest/accounts/#patch-api-v1-accounts-update-credentials
   Future<Account> updateCredentials({
     String displayName,
@@ -34,8 +98,38 @@ mixin Accounts on Authentication implements MockAccountsMixin {
     dynamic sourceSensitive,
     dynamic sourceLanguage,
     dynamic fieldsAttributes,
-  }) =>
-      throw UnimplementedError();
+  }) async {
+    assert(key != null);
+
+    final uri = Uri(
+        scheme: baseUrl.scheme,
+        host: baseUrl.host,
+        path: "/api/v1/accounts/verify_credentials",
+        queryParameters: {
+          "display_name": displayName,
+          "note": note,
+
+          /// TODO:  Avatar encoded using multipart/form-data
+          "avatar": null,
+
+          /// TODO: Header image encoded using multipart/form-data
+          "header": null,
+          "locked": locked == null ? null : locked.toString(),
+
+          /// TODO: implement source
+          "source": null,
+
+          /// TODO: implement fields_attributes
+          "fields_attributes": null,
+        });
+
+    final response = await patch(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Account.fromJson(json.decode(response.body));
+  }
 
   /// GET /api/v1/accounts/:id/followers
   /// https://docs.joinmastodon.org/api/rest/accounts/#get-api-v1-accounts-id-followers

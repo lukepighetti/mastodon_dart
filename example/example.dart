a43844c2b995b43cb99d64e010b324c4620a54c6
Luke Pighetti <lukepighetti@gmail.com>
2019-02-25 12:35:35 -0500

    example shows use of constructor

--- example/example.dart
+++ example/example.dart
@@ -3,11 +3,12 @@ import 'dart:io';
 import 'package:mastodon/mastodon.dart';
 
 main() async {
-  final mastodon = Mastodon()
-    ..baseUrl = Uri.parse(Platform.environment["BASE_URL"])
-    ..key = Platform.environment["KEY"];
+  final baseUrl = Uri.parse(Platform.environment["BASE_URL"]);
+  final key = Platform.environment["KEY"];
+
+  final mastodon = Mastodon(baseUrl)..key = key;
 
   final results = await mastodon.search("javascript");
 
-  print(results.statuses.map((s) => [s.account.displayName, s.content]));
+  print(results.statuses.map((s) => [s.tags.map((t) => t.name), s.content]));
 }

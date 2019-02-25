675171ed3b1449f61fe401a32390c055c3da9e4c
Luke Pighetti <lukepighetti@gmail.com>
2019-02-25 16:31:01 -0500

    implement Mutes

--- lib/src/mixins/mutes.dart
+++ lib/src/mixins/mutes.dart
@@ -9,7 +9,29 @@ mixin Mutes on Authentication implements MockMutesMixin {
   /// - read:mutes follow
   ///
   /// https://docs.joinmastodon.org/api/rest/mutes/#get-api-v1-mutes
-  Future<List<Account>> mutes({int limit = 40}) => throw UnimplementedError();
+  Future<List<Account>> mutes({int limit = 40}) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/mutes",
+      queryParameters: {
+        "limit": limit,
+      },
+    );
+
+    final response = await get(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    final body = List<Map>.from(json.decode(response.body));
+
+    /// TODO: implement link headers for pagination
+
+    return body.map((m) => Account.fromJson(m)).toList();
+  }
 
   /// POST /api/v1/accounts/:id/mute
   ///
@@ -17,7 +39,22 @@ mixin Mutes on Authentication implements MockMutesMixin {
   /// - write:mutes follow
   ///
   /// https://docs.joinmastodon.org/api/rest/mutes/#post-api-v1-accounts-id-mute
-  Future<Relationship> muteAccount(String id) => throw UnimplementedError();
+  Future<Relationship> muteAccount(String id) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/accounts/$id/mute",
+    );
+
+    final response = await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Relationship.fromJson(json.decode(response.body));
+  }
 
   /// POST /api/v1/accounts/:id/unmute
   ///
@@ -25,7 +62,22 @@ mixin Mutes on Authentication implements MockMutesMixin {
   /// - write:mutes follow
   ///
   /// https://docs.joinmastodon.org/api/rest/mutes/#post-api-v1-accounts-id-unmute
-  Future<Relationship> unmuteAccount(String id) => throw UnimplementedError();
+  Future<Relationship> unmuteAccount(String id) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/accounts/$id/unmute",
+    );
+
+    final response = await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Relationship.fromJson(json.decode(response.body));
+  }
 
   /// POST /api/v1/statuses/:id/mute
   ///
@@ -33,7 +85,22 @@ mixin Mutes on Authentication implements MockMutesMixin {
   /// - write write:mutes
   ///
   /// https://docs.joinmastodon.org/api/rest/mutes/#post-api-v1-statuses-id-mute
-  Future<Status> muteStatus(String id) => throw UnimplementedError();
+  Future<Status> muteStatus(String id) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/statuses/$id/mute",
+    );
+
+    final response = await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Status.fromJson(json.decode(response.body));
+  }
 
   /// POST /api/v1/statuses/:id/unmute
   ///
@@ -41,5 +108,20 @@ mixin Mutes on Authentication implements MockMutesMixin {
   /// - write write:mutes
   ///
   /// https://docs.joinmastodon.org/api/rest/mutes/#post-api-v1-statuses-id-unmute
-  Future<Status> unmuteStatus(String id) => throw UnimplementedError();
+  Future<Status> unmuteStatus(String id) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/statuses/$id/unmute",
+    );
+
+    final response = await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    return Status.fromJson(json.decode(response.body));
+  }
 }

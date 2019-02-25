806cf3f1051d5ba5569a961ba10c063362a4bc0b
Luke Pighetti <lukepighetti@gmail.com>
2019-02-25 14:20:37 -0500

    DomainBlocks

--- lib/src/mastodon.dart
+++ lib/src/mastodon.dart
@@ -4,12 +4,21 @@ import 'mixins/accounts.dart';
 import 'mixins/apps.dart';
 import 'mixins/blocks.dart';
 import 'mixins/custom_emojis.dart';
+import 'mixins/domain_blocks.dart';
 import 'mixins/search.dart';
 import 'mixins/statuses.dart';
 import 'mixins/timelines.dart';
 
 class Mastodon = Authentication
-    with Accounts, Apps, Blocks, CustomEmojis, Search, Statuses, Timelines;
+    with
+        Accounts,
+        Apps,
+        Blocks,
+        CustomEmojis,
+        DomainBlocks,
+        Search,
+        Statuses,
+        Timelines;
 
 class Authentication implements MockAuthentication {
   final Uri baseUrl;
---
+++ lib/src/mixins/domain_blocks.dart
@@ -0,0 +1,81 @@
+import '../library.dart';
+
+import 'package:mastodon/mock/mixins/domain_blocks.dart';
+
+mixin DomainBlocks on Authentication implements MockDomainBlocksMixin {
+  /// GET /api/v1/domain_blocks
+  ///
+  /// - authenticated (requires user)
+  /// - read read:blocks follow
+  ///
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#get-api-v1-domain-blocks
+  Future<List<Uri>> domainBlocks({int limit = 40}) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/domain_blocks",
+      queryParameters: {
+        "limit": limit,
+      },
+    );
+
+    final response = await get(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+
+    final body = List<String>.from(json.decode(response.body));
+
+    return body.map((s) => Uri.parse(s)).toList();
+  }
+
+  /// POST /api/v1/domain_blocks
+  ///
+  /// - authenticated (requires user)
+  /// - write write:blocks follow
+  ///
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#post-api-v1-domain-blocks
+  Future<dynamic> domainBlock(Uri domain) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/domain_blocks",
+      queryParameters: {
+        "domain": domain.toString(),
+      },
+    );
+
+    await post(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+  }
+
+  /// DELETE /api/v1/domain_blocks
+  ///
+  /// - authenticated (requires user)
+  /// - write write:blocks follow
+  ///
+  /// https://docs.joinmastodon.org/api/rest/domain-blocks/#delete-api-v1-domain-blocks
+  Future<dynamic> domainUnblock(Uri domain) async {
+    assert(key != null);
+
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/domain_blocks",
+      queryParameters: {
+        "domain": domain.toString(),
+      },
+    );
+
+    await delete(
+      uri,
+      headers: {"Authorization": "Bearer $key"},
+    );
+  }
+}

55b53824a45d3465d32458f96557b829a7d5b5cd
Luke Pighetti <lukepighetti@gmail.com>
2019-02-25 13:57:20 -0500

    CustomEmojis

--- lib/src/mastodon.dart
+++ lib/src/mastodon.dart
@@ -3,12 +3,13 @@ import 'package:mastodon/mock/mock_mastodon.dart';
 import 'mixins/accounts.dart';
 import 'mixins/apps.dart';
 import 'mixins/blocks.dart';
+import 'mixins/custom_emojis.dart';
 import 'mixins/search.dart';
 import 'mixins/statuses.dart';
 import 'mixins/timelines.dart';
 
 class Mastodon = Authentication
-    with Accounts, Apps, Blocks, Search, Statuses, Timelines;
+    with Accounts, Apps, Blocks, CustomEmojis, Search, Statuses, Timelines;
 
 class Authentication implements MockAuthentication {
   final Uri baseUrl;
---
+++ lib/src/mixins/custom_emojis.dart
@@ -0,0 +1,27 @@
+import '../library.dart';
+
+import 'package:mastodon/mock/mixins/custom_emojis.dart';
+
+mixin CustomEmojis on Authentication implements MockCustomEmojisMixin {
+  /// GET /api/v1/custom_emojis
+  ///
+  /// - public
+  /// - scope not specified
+  ///
+  /// https://docs.joinmastodon.org/api/rest/custom-emojis/#get-api-v1-custom-emojis
+  Future<List<Emoji>> emojis() async {
+    final uri = Uri(
+      scheme: baseUrl.scheme,
+      host: baseUrl.host,
+      path: "/api/v1/custom_emojis",
+    );
+
+    final response = await get(
+      uri,
+    );
+
+    final body = List<Map>.from(json.decode(response.body));
+
+    return body.map((m) => Emoji.fromJson(m)).toList();
+  }
+}

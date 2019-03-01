b59d9b7c51949fe18e60c958865265646bcf7ecb
Luke Pighetti <lukepighetti@gmail.com>
2019-03-01 17:05:47 -0500

    fix Validators.validateAuthCode guard against null values

--- lib/src/validators.dart
+++ lib/src/validators.dart
@@ -1,4 +1,5 @@
 mixin Validators {
   /// Makes sure the authentication code is 64 characters and alphanumeric.
-  validateAuthCode(String code) => RegExp(r"^[a-zA-Z0-9]{64}$").hasMatch(code);
+  validateAuthCode(String code) =>
+      RegExp(r"^[a-zA-Z0-9]{64}$").hasMatch(code ?? "");
 }

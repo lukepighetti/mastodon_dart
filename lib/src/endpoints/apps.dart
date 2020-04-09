f60fa06f076da9fb51240ccbc79f80c2ae61c563
GroovinChip <groovinchip@gmail.com>
2020-04-09 15:09:03 -0400

    update docs for Apps endpoint

--- lib/src/endpoints/apps.dart
+++ lib/src/endpoints/apps.dart
@@ -2,9 +2,15 @@ import '../library.dart';
 
 import '../../src/mock/endpoints/apps.dart';
 
+/// Register client applications that can be used to obtain OAuth tokens.
+/// https://docs.joinmastodon.org/methods/apps/
 mixin Apps on Authentication, Utilities implements MockApps {
+  /// Create a new application to obtain OAuth2 credentials.
+  ///
   /// POST /api/v1/apps
-  /// https://docs.joinmastodon.org/api/rest/apps/#post-api-v1-apps
+  ///
+  /// Error response types:
+  /// - 422: Unprocessable Entity (If a required parameter is missing or improperly formatted, the request will fail.)
   Future<AuthenticatedApplication> appCredentials(
     Uri website, [
     String redirectUris = "urn:ietf:wg:oauth:2.0:oob",
@@ -25,8 +31,13 @@ mixin Apps on Authentication, Utilities implements MockApps {
     return AuthenticatedApplication.fromJson(json.decode(response.body));
   }
 
+  /// Confirm that the app's OAuth2 credentials work.
+  ///
   /// GET /api/v1/apps/verify_credentials
-  /// https://docs.joinmastodon.org/api/rest/apps/#get-api-v1-apps-verify-credentials
+  ///
+  /// Error response types:
+  /// - 401: Unauthorized (If the Authorization header contains an invalid token,
+  /// is malformed, or is not present, an error will be returned indicating an authorization failure.)
   Future<Application> verifyAppCredentials() async {
     final response = await request(
       Method.post,

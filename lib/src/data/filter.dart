667decba830f4ddf421f6104611afd9a832586df
GroovinChip <groovinchip@gmail.com>
2020-04-09 14:27:04 -0400

    update docs for Filter

--- lib/src/data/filter.dart
+++ lib/src/data/filter.dart
@@ -14,16 +14,29 @@ part 'filter.g.dart';
   fieldRename: FieldRename.snake,
 )
 class Filter {
+  /// The ID of the filter in the database
   final String id;
+
+  /// The text to be filtered
   final String phrase;
+
+  /// The contexts in which the filter should be applied. Enumerated by [FilterContext].
+  /// - home = home timeline
+  /// - notifications = notifications timeline
+  /// - public = public timelines
+  /// - thread = expanded thread of a detailed status
   final List<FilterContext> context;
 
-  final bool wholeWord;
-
+  /// When the filter should no longer be applied
   @JsonKey(nullable: true)
   final DateTime expiresAt;
 
+  /// Should matching entities in home and notifications be dropped by the server?
   final bool irreversible;
+
+  /// Should the filter consider word boundaries?
+  /// See [implementation notes](https://docs.joinmastodon.org/entities/filter/) in Mastodon docs
+  final bool wholeWord;
 
   Filter({
     this.id,

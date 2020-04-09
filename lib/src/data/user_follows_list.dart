cdc6b964d5b1b6042bb0eae159b982bbeda3eb04
GroovinChip <groovinchip@gmail.com>
2020-04-09 14:35:33 -0400

    update docs for List

--- lib/src/data/user_follows_list.dart
+++ lib/src/data/user_follows_list.dart
@@ -4,6 +4,7 @@ import 'package:mastodon_dart/src/mock/properties.dart';
 part 'user_follows_list.g.dart';
 
 /// Represents a list of some users that the authenticated user follows.
+/// Cannot be named `List` because `List` is reserved in Dart.
 /// https://docs.joinmastodon.org/entities/list/
 
 @JsonSerializable(
@@ -12,7 +13,10 @@ part 'user_follows_list.g.dart';
   fieldRename: FieldRename.snake,
 )
 class UserFollowsList {
+  /// The internal database ID of the lis
   final String id;
+
+  /// The user-defined title of the list
   final String title;
 
   UserFollowsList({this.id, this.title});
